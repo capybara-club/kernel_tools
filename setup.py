4fd3571ba201cff35d9fed6e3d03a3b9cab2990f
Charles Durham <cpdurham@protonmail.com>
2024-05-21 15:42:00 -0400

    testing

--- setup.py
+++ setup.py
@@ -6,23 +6,26 @@ this_dir = os.path.dirname(os.path.curdir)
 library_name = "kernel_tools"
 extensions_dir = os.path.join(this_dir, library_name, "csrc")
 
-setup(
-    name='kernel_tools',
-    version='0.1',
-    description='PyTorch extension with C++ and CUDA',
-    packages=['kernel_tools'],
-    ext_modules=[
-        CUDAExtension(
-            name= "kernel_tools_cpp_cuda",
-            sources=[
-                'kernel_tools/csrc/tensor_test.cpp', 
-                'kernel_tools/csrc/bindings.cpp',
-            ],
-            include_dirs=['kernel_tools/csrc'],
-            extra_compile_args={'cxx': ['-g'], 'nvcc': ['-g']}
-        ),
-    ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
-)
+USE_JIT = os.getenv('USE_KERNEL_TOOLS_JIT', '0') == '1'
+
+if not USE_JIT:
+    setup(
+        name='kernel_tools',
+        version='0.1',
+        description='PyTorch extension with C++ and CUDA',
+        packages=['kernel_tools'],
+        ext_modules=[
+            CUDAExtension(
+                name= "kernel_tools_cpp_cuda",
+                sources=[
+                    'kernel_tools/csrc/tensor_test.cpp', 
+                    'kernel_tools/csrc/bindings.cpp',
+                ],
+                include_dirs=['kernel_tools/csrc'],
+                extra_compile_args={'cxx': ['-g'], 'nvcc': ['-g']}
+            ),
+        ],
+        cmdclass={
+            'build_ext': BuildExtension
+        }
+    )
