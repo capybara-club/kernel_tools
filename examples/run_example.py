5c2c37271c33bcf509efd28835bb4c147da7fe03
Charles Durham <cpdurham@protonmail.com>
2024-05-22 10:20:10 -0400

    clean

--- examples/run_example.py
+++ examples/run_example.py
@@ -1,62 +1,47 @@
 import torch
-import scipy as sp
 import time
+
+from scipy.linalg import eigh as scipy_eigh
 
 import kernel_tools.kernels as kernels
 from kernel_tools.linalg import cusolver_eigh
 
-torch.set_printoptions(linewidth=240)
+# torch.set_printoptions(linewidth=240)
 
 kernel_fn = lambda x, z: kernels.laplacian(x, z, bandwidth=20.)
 
-# warmup()
-# a = torch.arange(10)
-
-N = 20000
+N = 2000
 D = 2
-num_eigs = 100
+top_q_eig = 100
 
-a = torch.randn(N, D, dtype=torch.float32)
+a = torch.randn(N, D, dtype=torch.float64)
 
 kernel_mat = kernel_fn(a,a)
-print(kernel_mat)
 
-num_samples = N
-top_q_eig = num_eigs
+sub_start = N - top_q_eig - 1
+sub_end = N - 1
 
-scipy_sub_start = num_samples - top_q_eig - 1
-scipy_sub_end = num_samples - 1
-
-subset_by_index = (scipy_sub_start, scipy_sub_end)
+subset_by_index = (sub_start, sub_end)
 eigvals_only = False
 overwrite_a = True
 
-d = torch.zeros(N, dtype=kernel_mat.dtype)
-# mgSyevd(kernel_mat, d)
 kernel_mat_cuda = kernel_mat.to(device='cuda')
-# cuda_eigenvalues = syevdx(kernel_mat_cuda, cuda_sub_start, cuda_sub_end)
-# print(cuda_eigenvalues)
-
-# cuda_eigenvalues, cuda_eigenvectors = syevdx(kernel_mat.to(dtype=torch.float32), cuda_sub_start, cuda_sub_end)
-# print(cuda_eigenvalues)
-
-# print(kernel_mat_cuda)
-start = time.time()
-cuda_eigenvalues, cuda_eigenvectors = cusolver_eigh(kernel_mat_cuda.to(dtype=torch.float32), subset_by_index=subset_by_index, eigvals_only=eigvals_only, overwrite_a=overwrite_a)
-print(cuda_eigenvalues)
-end = time.time()
-print(end - start)
-
-# print(cuda_eigenvectors)
-
-# print(kernel_mat_cuda)
 kernel_mat_cpu = kernel_mat.cpu()
+
 start = time.time()
-scipy_eigenvalues, scipy_eigenvectors = sp.linalg.eigh(kernel_mat_cpu, subset_by_index=subset_by_index, eigvals_only=eigvals_only, overwrite_a=overwrite_a)
-print(scipy_eigenvalues)
+cuda_eigenvalues, cuda_eigenvectors = cusolver_eigh(kernel_mat_cuda, subset_by_index=subset_by_index, eigvals_only=eigvals_only, overwrite_a=overwrite_a)
 end = time.time()
+print('cuda eigenvalues:')
+print(cuda_eigenvalues)
+print('cuda eigenvectors:')
+print(cuda_eigenvectors)
 print(end - start)
-# print(scipy_eigenvectors)
-# print(kernel_mat_cpu)
-# print(kernel_mat)
-# print(d)
+
+start = time.time()
+scipy_eigenvalues, scipy_eigenvectors = scipy_eigh(kernel_mat_cpu, subset_by_index=subset_by_index, eigvals_only=eigvals_only, overwrite_a=overwrite_a)
+end = time.time()
+print('scipy eigenvalues:')
+print(scipy_eigenvalues)
+print('scipy eigenvectors:')
+print(scipy_eigenvectors)
+print(end - start)
