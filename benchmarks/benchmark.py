3d235198afcc2953322453408d198dce40481fba
Charles Durham <cpdurham@protonmail.com>
2024-05-22 14:09:51 -0400

    minor

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -29,6 +29,9 @@ def profile_cuda(N, num_eigs, dtype):
         return None
 
 def profile_scipy(N, num_eigs, dtype):
+    # dont even try
+    if N > 35_000:
+        return None
     a = torch.randn(N, D, dtype=dtype)
     kernel_mat = kernel_fn(a,a)
     sub_start = N - num_eigs - 1
@@ -43,12 +46,18 @@ def profile_scipy(N, num_eigs, dtype):
     end = time.time()
     return end - start
 
-n_sizes = [100, 1_000, 2_000, 5_000, 10_000, 15_000, 20_000, 25_000, 30_000, 35_000]
-n_eigs =  [ 10,    50,   100,   200,    500,    750,  1_000,  2_000, 3_000,   4_000]
+n_sizes = [100, 1_000, 2_000, 5_000, 10_000, 15_000, 20_000, 25_000, 30_000, 35_000, 40_000, 45_000, 50_000]
+n_eigs =  [ 10,    50,   100,   200,    500,    750,  1_000,  2_000,  3_000,  4_000,  5_000,  7_000,  9_000]
 n_dtypes = [torch.float32, torch.float64]
+
+# for dtype in n_dtypes:
+#     for N, num_eigs in list(zip(n_sizes, n_eigs)):
+#         scipy_time = profile_scipy(N, num_eigs, dtype)
+
+#         print(f'N:{N}, num_eigs:{num_eigs}, dtype:{dtype}, scipy_time:{scipy_time}')
 
 for dtype in n_dtypes:
     for N, num_eigs in list(zip(n_sizes, n_eigs)):
-        scipy_time = profile_scipy(N, num_eigs, dtype)
+        scipy_time = profile_cuda(N, num_eigs, dtype)
 
-        print(f'N:{N}, num_eigs:{num_eigs}, dtype:{dtype}, scipy_time:{scipy_time}')
+        print(f'N:{N}, num_eigs:{num_eigs}, dtype:{dtype}, cuda_time:{scipy_time}')
